3b458051a402248759505676198344a39b647400
benitaananias <benitaananias@gmail.com>
2026-04-30 21:16:39 +0100

    QuickMart_Sales.sql

--- SQL/QuickMart_Sales.sql
+++ SQL/QuickMart_Sales.sql
@@ -16,7 +16,7 @@ CREATE TABLE products (
     stock_quantity INT
 );
 
-CREATE TABLE Customers (
+CREATE TABLE customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(100),
     city VARCHAR(50),
